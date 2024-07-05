26516f4320a07098ad8f7dd95758c59c21935d6e
lucad-28 <lucadev.rll28@gmail.com>
2024-07-05 00:55:05 -0500

    commit

---
+++ Server/test_code.cpp
@@ -0,0 +1,35 @@
+int main(int a){
+    a=2;
+    int b = 5;
+    int c = 6;
+    int variable = 10;
+    //if ( (a<2) ** variable>10 || (b>10)) {
+    if ( (a<2) && variable>10 || (b>10)) {
+        a = 10 * 4 + 5;
+        while( a<10 && b>10 || c<10 ) {
+
+    }
+    }  else if (a<2){
+
+    } 
+    //while ( (a<2) && variable>10 || (b>10))) {
+    while( a<10 && b>10 || c<10 ) {
+
+    }
+    
+    /*else if (a<2){
+
+    }*/
+
+    arroba(a, b, c);
+    for (int i = 0; i < 5; i++){}
+    // Test exp math
+    int x = 10;
+    //x = (20 30)+55 - 4;
+    x = (20-30)+55 - 4;
+    //x = x *  + 5;
+    x = x * 3 + 5;
+    x = (40 + 55;
+    x = (40 + 55);
+}
+void arroba(int a, int b, int c){}
